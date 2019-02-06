ab6e2a831a5a212605e5962567fff74a186e092c
Manuel Nunez <03.manu@gmail.com>
2019-02-06 16:18:47 +0100

    Change static $table parameter in Reason model

--- app/Models/Reason.php
+++ app/Models/Reason.php
@@ -17,7 +17,7 @@ use Eloquent as Model;
 class Reason extends Model
 {
 
-    public static $table = 'reasons';
+    public $table = 'reasons';
     
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
