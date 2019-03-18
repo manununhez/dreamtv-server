faf2a805ea3b34e08b9e12efe3cc16a973e8d55e
Manuel Nunez <03.manu@gmail.com>
2019-03-18 20:44:43 +0100

    Migrations finished

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,12 +10,6 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
 
-	$router->get('/', 'HomeController@index');
-	$router->resource('db/useraccounts','UserAccountAdminController');
-	$router->resource('db/tasks','TaskAdminController');
-	$router->resource('db/reasons','ReasonAdminController');
-	$router->resource('db/usertasks','UserTaskAdminController');
-	$router->resource('db/uservideoslists','UserVideosListAdminController');
-	$router->resource('db/videotests','VideoTestsAdminController');
-	$router->resource('db/videos','VideoAdminController');
+    $router->get('/', 'HomeController@index');
+
 });
