14ae99f5955b3b547d236815d2ce517d459fbc28
Manuel Nunez <03.manu@gmail.com>
2019-03-18 19:13:44 +0100

    Migrations finished

--- database/migrations/2019_03_18_213400_create_user_tasks_errors_table.php
+++ database/migrations/2019_03_18_213400_create_user_tasks_errors_table.php
@@ -14,7 +14,7 @@ class CreateUserTasksErrorsTable extends Migration
     public function up()
     {
         Schema::create('user_tasks_errors', function (Blueprint $table) {
-            $table->primary(['user_id','task_id','subtitle_version','subtitle_position','reason_code']);
+            $table->primary(['user_id','task_id','subtitle_version','subtitle_position','reason_code'],'pk_user_tasks_errors');
             $table->integer('user_id');
             $table->integer('task_id');
             $table->string('subtitle_version');
