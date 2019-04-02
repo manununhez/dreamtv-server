3bc99ad3f04bde83140104678091e6a4af802470
Manuel Nunez <03.manu@gmail.com>
2019-04-02 19:07:44 +0200

    Base controller default message

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -34,7 +34,7 @@ class BaseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function sendError($error, $errorMessages = [], $code = 404)
+    public function sendError($error = 'Error', $errorMessages = [], $code = 404)
     {
         $response = [
             'success' => false,
