0d6d4a1c25cada4bf28165e4ed935a8a0cff8ffa
Manuel Nunez <03.manu@gmail.com>
2019-05-08 14:19:39 +0200

    Added [user_tasks_id] to fillable property to allow mass assignment on [App\\UserTaskError]

--- app/Models/UserTaskError.php
+++ app/Models/UserTaskError.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserTaskError extends Model
 {
-   //
+   protected $fillable = ['user_tasks_id','reason_code']; 
 
 	public function userTasks(){
         return $this->belongsTo(UserTask::class);
